fd599097b9e44fbb878f7802872e442a94def6a9
martaalcazarr <martaalcazarredondo@gmail.com>
2023-09-03 11:39:38 -0300

    add seed on prisma

---
+++ prisma/data/categories.ts
@@ -0,0 +1,31 @@
+const categories = [
+    {
+      icon: "coffee",
+      name: "Coffee"
+    },
+    {
+      icon: "hamburger",
+      name: "Burgers"
+    },
+    {
+      icon: "pizza",
+      name: "Pizzas"
+    },
+    {
+      icon: "donut",
+      name: "Donuts"
+    },
+    {
+      icon: "cake",
+      name: "Cakes"
+    },
+    {
+      icon: "cookies",
+      name: "Cookies"
+    }
+  ]
+  
+  export {
+    categories
+  }
+  
