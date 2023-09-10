47088ed9afe4a391df7c9aae3841ce712c337972
martaalcazarr <martaalcazarredondo@gmail.com>
2023-09-10 14:46:21 -0300

    add choose quantity for product

--- components/ModalProduct.js
+++ components/ModalProduct.js
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import Image from "next/image"
 import useStore from "../hooks/useStore"
 import { formatMoney } from "../helpers"
@@ -5,6 +6,7 @@ import { formatMoney } from "../helpers"
 const ModalProduct = () => {
 
     const {product, handleChangeModal} = useStore()
+    const [quantity, setQuantity] = useState(1)
 
 
   return (
@@ -34,6 +36,36 @@ const ModalProduct = () => {
             <p className="mt-5 font-black text-5xl text-amber-500">
                 {formatMoney(product.price)}
             </p>
+
+            <div className="flex gap-4 mt-5">
+                <button
+                    type="button"
+                    onClick={() => {
+                        if(quantity <= 1 ) return
+                        setQuantity(quantity - 1)
+                    }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+
+
+                </button>
+
+                <p className="text-3xl ">{quantity}</p>    
+                <button
+                    type="button"
+                    onClick={() => {
+                        if(quantity >= 30) return
+                        setQuantity(quantity + 1)
+                    }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-7 h-7">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    </svg>
+
+                </button>
+            </div>
         </div>
     </div>
     
