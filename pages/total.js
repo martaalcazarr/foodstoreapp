6c408ec8f6c7e22cca6bc29587907ffdec210e18
martaalcazarr <martaalcazarredondo@gmail.com>
2023-09-23 18:00:52 -0300

    add total page

--- pages/total.js
+++ pages/total.js
@@ -5,6 +5,40 @@ export default function Total(){
         <Layout page='Total and confirm order'>
         <h1 className="text-4xl font-black">Total</h1>
         <p className="text-2xl my-10">Confirm your order</p>
+
+        <form>
+        <div>
+            <label
+                htmlFor="name"
+                className="block uppercase text-slate-800 font-bold text-xl">
+                Name
+            </label>
+
+            <input
+                id="name" 
+                type="text"
+                className="bg-gray-200 w-full lg:w-1/3 mt-3 p-2 rounded-md"
+            />
+        </div>
+
+        <div className="mt-10">
+            <p className="text-2xl">Total {''}
+            <span className="font-bold">
+                300
+            </span>
+            </p>
+
+        </div>
+
+        <div className="mt-5">
+            <input 
+                className="text-center w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-white bg-indigo-600"
+                value="Confirm order"
+            />
+        </div>
+    </form>
     </Layout>
+
+    
     )
 }
